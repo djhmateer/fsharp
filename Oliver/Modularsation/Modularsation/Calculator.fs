f700e599949043abc6eefbd93374b54ef026cc87
Dave Mateer <davemateer@gmail.com>
2015-09-25 05:31:18 +0100

    modules

--- Oliver/Modularsation/Modularsation/Calculator.fs
+++ Oliver/Modularsation/Modularsation/Calculator.fs
@@ -1,8 +1,13 @@
-﻿module Calculator
+﻿// specific
+//namespace global
+namespace Mateer.Demo.Calculator
 
-let square x = x * x
+// need an = and indents when multiple modules in 1 file
+module Adder =
+    let square x = x * x
 
-let add x y = x + y
+    let add x y = x + y
 
-let mult x y = x * y
+module Multiplier =
+    let mult x y = x * y
 
--- Oliver/Modularsation/Modularsation/Program.fs
+++ Oliver/Modularsation/Modularsation/Program.fs
@@ -1,2 +1,16 @@
-﻿
-printfn "add 5 and 3 is %d" (Calculator.add 5 3)
+﻿//module Program
+// Declaring myself in the same namespace
+// so don't need to do an open
+module Mateer.Demo.Calculator.Program
+
+
+// open namespace
+//open Mateer.Demo.Calculator
+// open the module!
+//open Mateer.Demo.Calculator.Adder
+
+[<EntryPoint>]
+let main args =
+    printfn "add 5 and 3 is %d" (Adder.add 5 3)
+
+    0
