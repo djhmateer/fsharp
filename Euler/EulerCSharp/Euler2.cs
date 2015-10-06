f08895367cc334dddb889251aed88d946377793e
Dave Mateer <davemateer@gmail.com>
2015-10-06 17:10:09 +0100

    Euler2 F# and C# interesting functional style

--- Euler/EulerCSharp/Euler2.cs
+++ Euler/EulerCSharp/Euler2.cs
@@ -1,38 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace EulerCSharp
 {
     class Euler2
     {
+        //http://basildoncoder.com/blog/project-euler-problems-1-and-2.html
         static void Main()
         {
-            //Calculate the sum of all even Fibonacci numbers that are less than 4 million
-            int previousValue = 0, newValue = 1;
-            int answer = 0;
+            var result = Fibs().TakeWhile(f => f < 4000000).Where(f => f % 2 == 0).Sum();
+            Console.WriteLine(result);
+        }
 
+        static IEnumerable<long> Fibs()
+        {
+            long a = 0, b = 1;
             while (true)
             {
-                //Generate the next number in the sequence
-                int tempValue = previousValue + newValue;
-
-                //Check it's still less than 4 million
-                if (tempValue > 4000000)
-                    break;
-
-                //Check if it's even, if it is add it to the answer value
-                if (tempValue % 2 == 0)
-                    answer += tempValue;
-
-                //Move the values around so that we don't store anything more than the last two numbers in the sequence 
-                previousValue = newValue;
-                newValue = tempValue;
+                yield return b;
+                b = a + b;
+                a = b - a;
             }
-
-            Console.WriteLine("Answer is: {0}", answer);
-            Console.WriteLine($"Answer is: {answer}");
-            Console.ReadLine();
-
         }
     }
 }
